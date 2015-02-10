05c1e26681d6f3f53edb252b936558a2f3129798
markshen <markshen@markshen-Inspiron-1525.(none)>
2015-02-10 07:49:28 -0500

    linkedlist definition update

--- cpp/linkedlist/linkedlist.h
+++ cpp/linkedlist/linkedlist.h
@@ -1,7 +1,20 @@
+#ifndef _LINKEDLIST_H_
+#define _LINKEDLIST_H_
 
-template<T>
+template<class T>
 class LinkedList
 {
+private:	
+    template<class N>
+	class Node{
+		Node<N>(N& data) :mNext(nullptr){ mData = data; };
+		~Node<N>() { delete mData; mNext = nullptr; };
+		
+		N mData;
+		Node* mNext;		
+	};
+    
+	
 public:
     LinkedList<T>();
     ~LinkedList<T>();
@@ -10,4 +23,6 @@ public:
 
 
 
-}
+};
+
+#endif
